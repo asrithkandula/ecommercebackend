35e925dca40b8db07e76a3242d18017ad5ea24f2
Kandula Asrith <kandulaasrith@gmail.com>
2024-07-10 16:52:06 +0530

    first

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const conn = await mongoose.connect("mongodb+srv://kandulaasrith:Assg1234@cluster0.65moabd.mongodb.net/Project0?retryWrites=true&w=majority&appName=Cluster0")
         console.log("Mongo DB Connected: ", conn.connection.host)
     }
     catch(err) {
